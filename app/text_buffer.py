851e4028016c43baf208f2462f8f8d7236478d51
Dave Schuyler <dschuyler@chromium.org>
2016-10-31 10:32:35 -0700

    adjust marker and cursor when they cross in word selection

--- app/text_buffer.py
+++ app/text_buffer.py
@@ -1111,24 +1111,28 @@ class BackingTextBuffer(Mutator):
       return
     row = max(0, min(self.scrollRow + row, len(self.lines) - 1))
     col = max(0, min(self.scrollCol + col, len(self.lines[row])))
+    # Adjust the marker column delta when the cursor and marker positions
+    # cross over each other.
     markerCol = 0
     if self.cursorRow == self.markerRow:
       if row == self.cursorRow:
         if self.cursorCol > self.markerCol and col < self.markerCol:
-          self.prg.log('@@@@ left')
           markerCol = 1
-        if self.cursorCol < self.markerCol and col > self.markerCol:
-          self.prg.log('@@@@ right')
+        elif self.cursorCol < self.markerCol and col >= self.markerCol:
           markerCol = -1
       else:
         if (row < self.cursorRow and
             self.cursorCol > self.markerCol):
-          self.prg.log('@@@@ up')
           markerCol = 1
-        if (row > self.cursorRow and
+        elif (row > self.cursorRow and
             self.cursorCol < self.markerCol):
-          self.prg.log('@@@@ down')
           markerCol = -1
+    elif row == self.markerRow:
+      if col < self.markerCol and row < self.cursorRow:
+        markerCol = 1
+      elif col >= self.markerCol and row > self.cursorRow:
+        markerCol = -1
+
     self.cursorMoveAndMark(row - self.cursorRow, col - self.cursorCol,
         col - self.goalCol, 0, markerCol, 0)
     self.redo()
