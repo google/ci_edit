89906ff5eb2890a48d469c1ff0a9de2b2b8c6e4b
Dave Schuyler <dschuyler@chromium.org>
2016-11-15 19:16:35 -0800

    selectable tests, wip

--- tests/selectable_tests.py
+++ tests/selectable_tests.py
@@ -1,10 +1,9 @@
-
-import app.text_buffer
+import app.selectable
 import unittest
 
 class SelectableTestCases(unittest.TestCase):
   def setUp(self):
-    self.selectable = app.text_buffer.Selectable()
+    self.selectable = app.selectable.Selectable()
 
   def tearDown(self):
     self.selectable = None
@@ -16,8 +15,18 @@ class SelectableTestCases(unittest.TestCase):
   def test_selection_all(self):
     selectable = self.selectable
     selectable.lines = ['one', '', 'five']
-    selectable.selectionMode = app.text_buffer.kSelectionAll
+    selectable.selectionMode = app.selectable.kSelectionAll
     self.assertEqual(selectable.extendSelection(), (2, 4, 4, 0, 0, 0))
+
+  def test_selection_word(self):
+    selectable = self.selectable
+    selectable.lines = ['one two', '', 'five']
+    mode = app.selectable.kSelectionWord
+    selectable.selectionMode = mode
+    self.assertEqual(selectable.extendSelection(), (0, 7, 7, 0, 0, mode))
+    selectable.cursorRow = 2
+    selectable.extendSelection()
+    self.assertEqual(selectable.extendSelection(), (2, 4, 4, 2, 0, mode))
 
 if __name__ == '__main__':
   unittest.main()
