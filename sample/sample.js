adf649956141b9b8a0992939a3e64bad9b817637
Dave Schuyler <dschuyler@chromium.org>
2017-01-26 10:54:15 -0800

    improve highlighting

---
+++ sample/sample.js
@@ -0,0 +1,16 @@
+
+
+/* normal comment @param */
+
+/** doc comment
+ * @param
+ * @return {boolean}
+ */
+// line comment @param
+# not a comment
+<!-- not a comment -->
+
+
+function foo() {
+  if or while or switch
+}
