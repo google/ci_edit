01565df04404a984d2f3d44f4f0f9831e900812a
Dave Schuyler <dschuyler@chromium.org>
2017-02-01 19:03:07 -0800

    added to sample

--- sample/sample.js
+++ sample/sample.js
@@ -1,16 +1,20 @@
 
 
 /* normal comment @param */
+# not a comment
 
 /** doc comment
  * @param
  * @return {boolean}
  */
+namespace is not a keyword
 // line comment @param
-# not a comment
 <!-- not a comment -->
 
 
 function foo() {
   if or while or switch
 }
+
+'sadfsds\nsadf' //
+"wfefw\\ \" safsf" //
