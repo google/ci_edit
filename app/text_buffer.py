26a1e8c03a1c31fb11ef3fc46328e6b4392fed93
Dave Schuyler <dschuyler@chromium.org>
2017-02-28 19:28:06 -0800

    handle file does not exist in isSafeToWrite

--- app/text_buffer.py
+++ app/text_buffer.py
@@ -85,6 +85,8 @@ class Mutator(app.selectable.Selectable):
     return not clean
 
   def isSafeToWrite(self):
+    if not os.path.exists(self.fullPath):
+      return True
     s1 = os.stat(self.fullPath)
     s2 = self.fileStat
     app.log.info('st_mode', s1.st_mode, s2.st_mode)
