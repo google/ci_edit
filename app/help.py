40fc40f02c5c1e6d9f9dd2f298a4f8a3821a0c42
Dave Schuyler <dschuyler@chromium.org>
2017-03-17 10:13:35 -0700

    b7

--- app/help.py
+++ app/help.py
@@ -76,7 +76,7 @@ Within the Goto line prompt:
 
   'version': \
 """
-  Version: b6
+  Version: b7
   See LICENSE for license information
   See readme.md for an introduction
   Both files may be found in "%s"
