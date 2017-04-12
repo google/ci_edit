c232cd79c2b9176e40f3812b9709762fc38f653e
Dave Schuyler <dschuyler@chromium.org>
2017-04-12 11:03:56 -0700

    release test; b14

--- app/help.py
+++ app/help.py
@@ -76,7 +76,7 @@ Within the Goto line prompt:
 
   'version': \
 """
-  Version: b13
+  Version: b14
   See LICENSE for license information
   See readme.md for an introduction
   Both files may be found in "%s"
