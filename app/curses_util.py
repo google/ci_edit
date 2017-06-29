24275f29d88648e7ef7cbae0e76f2fc979633da9
Dave Schuyler <dschuyler@chromium.org>
2017-06-28 19:25:26 -0700

    change curses util comments and var names

--- app/curses_util.py
+++ app/curses_util.py
@@ -25,45 +25,45 @@ import curses.ascii
 # Strings are found using the cursesKeyName() function.
 # Constants are found using the curses.getch() function.
 
-CTRL_AT = '^@' #0x00
-CTRL_SPACE = '^@' #0x00
-CTRL_A = '^A' #0x01
-CTRL_B = '^B' #0x02
-CTRL_C = '^C' #0x03
-CTRL_D = '^D' #0x04
-CTRL_E = '^E' #0x05
-CTRL_F = '^F' #0x06
-CTRL_G = '^G' #0x07
-CTRL_H = '^H' #0x08
-CTRL_I = '^I' #0x09
-CTRL_J = '^J' #0x0a
-CTRL_K = '^K' #0x0b
-CTRL_L = '^L' #0x0c
-CTRL_M = '^M' #0x0d
-CTRL_N = '^N' #0x0e
-CTRL_O = '^O' #0x0f
-CTRL_P = '^P' #0x10
-CTRL_Q = '^Q' #0x11
-CTRL_R = '^R' #0x12
-CTRL_S = '^S' #0x13
-CTRL_T = '^T' #0x14
-CTRL_U = '^U' #0x15
-CTRL_V = '^V' #0x16
-CTRL_W = '^W' #0x17
-CTRL_X = '^X' #0x18
-CTRL_Y = '^Y' #0x19
-CTRL_Z = '^Z' #0x1a
-CTRL_OPEN_BRACKET = '^[' #0x1b
-CTRL_BACKSLASH = '^\\' #0x1c
-CTRL_CLOSE_BRACKET = '^]' #0x1d
-CTRL_CARROT = '^^' #0x1e
-CTRL_UNDERBAR = '^_' #0x1f
+CTRL_AT = '^@'  # 0x00
+CTRL_SPACE = '^@'  # 0x00
+CTRL_A = '^A'  # 0x01
+CTRL_B = '^B'  # 0x02
+CTRL_C = '^C'  # 0x03
+CTRL_D = '^D'  # 0x04
+CTRL_E = '^E'  # 0x05
+CTRL_F = '^F'  # 0x06
+CTRL_G = '^G'  # 0x07
+CTRL_H = '^H'  # 0x08
+CTRL_I = '^I'  # 0x09
+CTRL_J = '^J'  # 0x0a
+CTRL_K = '^K'  # 0x0b
+CTRL_L = '^L'  # 0x0c
+CTRL_M = '^M'  # 0x0d
+CTRL_N = '^N'  # 0x0e
+CTRL_O = '^O'  # 0x0f
+CTRL_P = '^P'  # 0x10
+CTRL_Q = '^Q'  # 0x11
+CTRL_R = '^R'  # 0x12
+CTRL_S = '^S'  # 0x13
+CTRL_T = '^T'  # 0x14
+CTRL_U = '^U'  # 0x15
+CTRL_V = '^V'  # 0x16
+CTRL_W = '^W'  # 0x17
+CTRL_X = '^X'  # 0x18
+CTRL_Y = '^Y'  # 0x19
+CTRL_Z = '^Z'  # 0x1a
+CTRL_OPEN_BRACKET = '^['  # 0x1b
+CTRL_BACKSLASH = '^\\'  # 0x1c
+CTRL_CLOSE_BRACKET = '^]'  # 0x1d
+CTRL_CARROT = '^^'  # 0x1e
+CTRL_UNDERBAR = '^_'  # 0x1f
 
 KEY_ESCAPE = curses.ascii.ESC
 
-KEY_BACKSPACE1 = curses.ascii.BS # 8
-KEY_BACKSPACE2 = curses.ascii.DEL # 127
-KEY_BACKSPACE3 = curses.KEY_BACKSPACE #263
+KEY_BACKSPACE1 = curses.ascii.BS  # 8
+KEY_BACKSPACE2 = curses.ascii.DEL  # 127
+KEY_BACKSPACE3 = curses.KEY_BACKSPACE  # 263
 KEY_DELETE = curses.KEY_DC
 KEY_HOME = curses.KEY_HOME
 KEY_END = curses.KEY_END
@@ -80,33 +80,27 @@ KEY_ALT_S = 159
 if sys.platform == 'darwin':
   KEY_ALT_LEFT = (91, 49, 59, 57, 68)
   KEY_ALT_RIGHT = (91, 49, 59, 57, 67)
-  KEY_ALT_SHIFT_LEFT = (None,)  # unknown
-  KEY_ALT_SHIFT_RIGHT = (None,)  # unknown
-  KEY_CTRL_DOWN = 521
-  KEY_CTRL_SHIFT_DOWN = 522
-  KEY_CTRL_LEFT = 541
-  KEY_CTRL_SHIFT_LEFT = 542
-  KEY_CTRL_RIGHT = 556
-  KEY_CTRL_SHIFT_RIGHT = 557
-  KEY_CTRL_UP = 562
-  KEY_CTRL_SHIFT_UP = 563
+  KEY_ALT_SHIFT_LEFT = (91, 49, 59, 49, 48, 68,)
+  KEY_ALT_SHIFT_RIGHT = (91, 49, 59, 49, 48, 67,)
 else:
   KEY_ALT_LEFT = 'kLFT3'
   KEY_ALT_RIGHT = 'kRIT3'
   KEY_ALT_SHIFT_LEFT = 'kLFT4'
   KEY_ALT_SHIFT_RIGHT = 'kRIT4'
-  KEY_CTRL_DOWN = 'kDN5'
-  KEY_CTRL_SHIFT_DOWN = 'kDN6'
-  KEY_CTRL_LEFT = 'kLFT5'
-  KEY_CTRL_SHIFT_LEFT = 'kLFT6'
-  KEY_CTRL_RIGHT = 'kRIT5'
-  KEY_CTRL_SHIFT_RIGHT = 'kRIT6'
-  KEY_CTRL_UP = 'kUP5'
-  KEY_CTRL_SHIFT_UP = 'kUP6'
 
 if 'SSH_CLIENT' in os.environ:
   KEY_ALT_LEFT = (98,)  # Need a better way to sort this out.
   KEY_ALT_RIGHT = (102,)  # ditto
+
+
+KEY_CTRL_DOWN = 'kDN5'
+KEY_CTRL_SHIFT_DOWN = 'kDN6'
+KEY_CTRL_LEFT = 'kLFT5'
+KEY_CTRL_SHIFT_LEFT = 'kLFT6'
+KEY_CTRL_RIGHT = 'kRIT5'
+KEY_CTRL_SHIFT_RIGHT = 'kRIT6'
+KEY_CTRL_UP = 'kUP5'
+KEY_CTRL_SHIFT_UP = 'kUP6'
 
 KEY_F1 = curses.KEY_F1
 KEY_F2 = curses.KEY_F2
@@ -138,53 +132,53 @@ KEY_SHIFT_LEFT = curses.KEY_SLEFT
 KEY_RIGHT = curses.KEY_RIGHT
 KEY_SHIFT_RIGHT = curses.KEY_SRIGHT
 
-def mouseButtonName(bstate):
+def mouseButtonName(buttonState):
   """Curses debugging. Prints readable name for state of mouse buttons."""
   result = ''
-  if bstate & curses.BUTTON1_RELEASED:
+  if buttonState & curses.BUTTON1_RELEASED:
     result += 'BUTTON1_RELEASED'
-  if bstate & curses.BUTTON1_PRESSED:
+  if buttonState & curses.BUTTON1_PRESSED:
     result += 'BUTTON1_PRESSED'
-  if bstate & curses.BUTTON1_CLICKED:
+  if buttonState & curses.BUTTON1_CLICKED:
     result += 'BUTTON1_CLICKED'
-  if bstate & curses.BUTTON1_DOUBLE_CLICKED:
+  if buttonState & curses.BUTTON1_DOUBLE_CLICKED:
     result += 'BUTTON1_DOUBLE_CLICKED'
 
-  if bstate & curses.BUTTON2_RELEASED:
+  if buttonState & curses.BUTTON2_RELEASED:
     result += 'BUTTON2_RELEASED'
-  if bstate & curses.BUTTON2_PRESSED:
+  if buttonState & curses.BUTTON2_PRESSED:
     result += 'BUTTON2_PRESSED'
-  if bstate & curses.BUTTON2_CLICKED:
+  if buttonState & curses.BUTTON2_CLICKED:
     result += 'BUTTON2_CLICKED'
-  if bstate & curses.BUTTON2_DOUBLE_CLICKED:
+  if buttonState & curses.BUTTON2_DOUBLE_CLICKED:
     result += 'BUTTON2_DOUBLE_CLICKED'
 
-  if bstate & curses.BUTTON3_RELEASED:
+  if buttonState & curses.BUTTON3_RELEASED:
     result += 'BUTTON3_RELEASED'
-  if bstate & curses.BUTTON3_PRESSED:
+  if buttonState & curses.BUTTON3_PRESSED:
     result += 'BUTTON3_PRESSED'
-  if bstate & curses.BUTTON3_CLICKED:
+  if buttonState & curses.BUTTON3_CLICKED:
     result += 'BUTTON3_CLICKED'
-  if bstate & curses.BUTTON3_DOUBLE_CLICKED:
+  if buttonState & curses.BUTTON3_DOUBLE_CLICKED:
     result += 'BUTTON3_DOUBLE_CLICKED'
 
-  if bstate & curses.BUTTON4_RELEASED:
+  if buttonState & curses.BUTTON4_RELEASED:
     result += 'BUTTON4_RELEASED'
-  if bstate & curses.BUTTON4_PRESSED:
+  if buttonState & curses.BUTTON4_PRESSED:
     result += 'BUTTON4_PRESSED'
-  if bstate & curses.BUTTON4_CLICKED:
+  if buttonState & curses.BUTTON4_CLICKED:
     result += 'BUTTON4_CLICKED'
-  if bstate & curses.BUTTON4_DOUBLE_CLICKED:
+  if buttonState & curses.BUTTON4_DOUBLE_CLICKED:
     result += 'BUTTON4_DOUBLE_CLICKED'
 
-  if bstate & curses.REPORT_MOUSE_POSITION:
+  if buttonState & curses.REPORT_MOUSE_POSITION:
     result += 'REPORT_MOUSE_POSITION'
 
-  if bstate & curses.BUTTON_SHIFT:
+  if buttonState & curses.BUTTON_SHIFT:
     result += ' SHIFT'
-  if bstate & curses.BUTTON_CTRL:
+  if buttonState & curses.BUTTON_CTRL:
     result += ' CTRL'
-  if bstate & curses.BUTTON_ALT:
+  if buttonState & curses.BUTTON_ALT:
     result += ' ALT'
   return result
 
