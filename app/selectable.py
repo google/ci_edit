23538a4e200f86bf4f57309bdd2b7415ae0e1892
Dave Schuyler <dschuyler@chromium.org>
2017-10-23 19:14:23 -0700

    fix highlighting caps-follow-by-s matching

--- app/selectable.py
+++ app/selectable.py
@@ -24,7 +24,7 @@ kReStrings = re.compile(
     r"(\"\"\".*?(?<!\\)\"\"\")|('''.*?(?<!\\)''')|(\".*?(?<!\\)\")|('.*?(?<!\\)')")
 # The first group is a hack to allow upper case pluralized, e.g. URLs.
 kReSubwords = re.compile(
-    '(?:[A-Z]+s)|(?:[A-Z][a-z]+)|(?:[A-Z]+(?![a-z]))|(?:[a-z]+)')
+    r'(?:[A-Z]{2,}s\b)|(?:[A-Z][a-z]+)|(?:[A-Z]+(?![a-z]))|(?:[a-z]+)')
 kReSubwordBoundaryFwd = re.compile(
     '(?:[_-]?[A-Z][a-z-]+)|(?:[_-]?[A-Z]+(?![a-z]))|(?:[_-]?[a-z]+)|(?:\W+)')
 kReSubwordBoundaryRvr = re.compile(
