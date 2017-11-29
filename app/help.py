e05d9af67e48d801d70ee4756acd051ce3084157
Dave Schuyler <dschuyler@chromium.org>
2017-11-29 10:35:46 -0800

    v32

--- app/help.py
+++ app/help.py
@@ -121,7 +121,7 @@ Within the Goto line prompt:
 
   'version': \
 """\
-  Version (build iteration): v31
+  Version (build iteration): v32
   See LICENSE for license information
   See readme.md for an introduction
   Both files may be found in "%s"
