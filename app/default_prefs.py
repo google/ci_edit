d66af9037e957d3224b177d787e59fe7f73a6421
Aaron Xu <aaron_rg_xu@hotmail.com>
2017-12-06 11:57:01 -0800

    Changed the color for bookmarks. Maybe want to use alternating colors to differentiate different bookmarks

--- app/default_prefs.py
+++ app/default_prefs.py
@@ -154,7 +154,7 @@ for i in color8.values():
 
 color256 = {
   '_pre_selection': stringColorIndex,
-  'bookmark': 10,
+  'bookmark': 98,
   'bracket': 6,
   'c': defaultColorIndex,
   'c_preprocessor': 1,
