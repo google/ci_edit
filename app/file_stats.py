4afcef4d3204f47d6cffa8d2da3d9fdd3e80467a
Aaron Xu <aaron_rg_xu@hotmail.com>
2017-12-24 17:56:07 -0800

    Added check that prevents this function from breaking before some attributes are initialized

--- app/file_stats.py
+++ app/file_stats.py
@@ -137,22 +137,23 @@ class FileStats:
     Returns:
       True if the file on disk has changed. Otherwise, False.
     """
-    self.updateStats()
-    s1 = self.fileStats
-    s2 = self.savedFileStat
-    app.log.info('st_mode', s1.st_mode, s2.st_mode)
-    app.log.info('st_ino', s1.st_ino, s2.st_ino)
-    app.log.info('st_dev', s1.st_dev, s2.st_dev)
-    app.log.info('st_uid', s1.st_uid, s2.st_uid)
-    app.log.info('st_gid', s1.st_gid, s2.st_gid)
-    app.log.info('st_size', s1.st_size, s2.st_size)
-    app.log.info('st_mtime', s1.st_mtime, s2.st_mtime)
-    app.log.info('st_ctime', s1.st_ctime, s2.st_ctime)
-    return not (s1.st_mode == s2.st_mode and
-                s1.st_ino == s2.st_ino and
-                s1.st_dev == s2.st_dev and
-                s1.st_uid == s2.st_uid and
-                s1.st_gid == s2.st_gid and
-                s1.st_size == s2.st_size and
-                s1.st_mtime == s2.st_mtime and
-                s1.st_ctime == s2.st_ctime)
+    if (self.updateStats() and self.fileStats):
+      s1 = self.fileStats
+      s2 = self.savedFileStat
+      app.log.info('st_mode', s1.st_mode, s2.st_mode)
+      app.log.info('st_ino', s1.st_ino, s2.st_ino)
+      app.log.info('st_dev', s1.st_dev, s2.st_dev)
+      app.log.info('st_uid', s1.st_uid, s2.st_uid)
+      app.log.info('st_gid', s1.st_gid, s2.st_gid)
+      app.log.info('st_size', s1.st_size, s2.st_size)
+      app.log.info('st_mtime', s1.st_mtime, s2.st_mtime)
+      app.log.info('st_ctime', s1.st_ctime, s2.st_ctime)
+      return not (s1.st_mode == s2.st_mode and
+                  s1.st_ino == s2.st_ino and
+                  s1.st_dev == s2.st_dev and
+                  s1.st_uid == s2.st_uid and
+                  s1.st_gid == s2.st_gid and
+                  s1.st_size == s2.st_size and
+                  s1.st_mtime == s2.st_mtime and
+                  s1.st_ctime == s2.st_ctime)
+    return False
