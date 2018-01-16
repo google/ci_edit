3a4fa7c59540809b07e2ec5a033dcea37cd83eb2
Aaron Xu <aaron_rg_xu@hotmail.com>
2018-01-16 15:35:29 -0800

    Added more tests and found a bug which needs to be fixed

--- app/unit_test_bookmarks.py
+++ app/unit_test_bookmarks.py
@@ -1,19 +1,27 @@
 import app.prefs
 import app.text_buffer
 import app.window
-from mock import patch, Mock
+import mock
 import unittest
 
 class BookmarkTestCases(unittest.TestCase):
   def setUp(self):
+    self.fakeHost = mock.Mock()
     self.textBuffer = app.text_buffer.TextBuffer()
+    self.textBuffer.lines = 50
+    self.lineNumbers = app.window.LineNumbers(self.fakeHost)
+    self.lineNumbers.rows = 30
+    self.lineNumbers.parent = self.fakeHost
+    self.fakeHost.lineNumberColumn = self.lineNumbers
+    self.fakeHost.textBuffer = self.textBuffer
+    self.fakeHost.scrollRow = self.fakeHost.cursorRow = 0
 
   def tearDown(self):
     pass
 
   def test_get_next_bookmark_color(self):
     def test_with_an_x_colored_terminal(x):
-      patch.dict(app.prefs.startup, {'numColors': x}, clear=True)
+      mock.patch.dict(app.prefs.startup, {'numColors': x}, clear=True)
       colors = set()
       expectedNumberOfColors = 5
       for _ in range(expectedNumberOfColors):
@@ -36,20 +44,55 @@ class BookmarkTestCases(unittest.TestCase):
 
   def test_get_visible_bookmarks(self):
     # Set up mock objects to test the LineNumbers methods.
-    fakeHost = Mock()
-    lineNumbers = app.window.LineNumbers(fakeHost)
-    fakeHost.lineNumberColumn = lineNumbers
-    lineNumbers.parent = fakeHost
-    fakeHost.textBuffer = Mock()
-    fakeHost.textBuffer.lines = 50
-    fakeHost.scrollRow = fakeHost.cursorRow = 0
-    lineNumbers.rows = 30
-    fakeHost.textBuffer.bookmarks = [((0, 0),), ((10, 10),), ((20, 20),),
-                                     ((30, 30),), ((30, 30),),]
-    visibleBookmarks = lineNumbers.getVisibleBookmarks(0, 30)
+
+    self.textBuffer.bookmarks = [((0, 0),), ((10, 10),), ((20, 20),),
+                                 ((30, 30),), ((40, 40),),]
+    visibleBookmarks = self.lineNumbers.getVisibleBookmarks(
+        self.fakeHost.scrollRow, self.lineNumbers.rows)
     expectedBookmarks = {((0, 0),), ((10, 10),), ((20, 20),)}
     self.assertEqual(set(visibleBookmarks), expectedBookmarks)
-    self.assertEqual(len(visibleBookmarks), 3)
+    self.assertEqual(len(visibleBookmarks), len(expectedBookmarks))
+
+    self.fakeHost.scrollRow = 20
+    visibleBookmarks = self.lineNumbers.getVisibleBookmarks(
+        self.fakeHost.scrollRow, 20 + self.lineNumbers.rows)
+    expectedBookmarks = {((20, 20),), ((30, 30),), ((40, 40),)}
+    self.assertEqual(set(visibleBookmarks), expectedBookmarks)
+    self.assertEqual(len(visibleBookmarks), len(expectedBookmarks))
+
+    self.fakeHost.scrollRow = 21
+    visibleBookmarks = self.lineNumbers.getVisibleBookmarks(
+        self.fakeHost.scrollRow, self.fakeHost.scrollRow + self.lineNumbers.rows)
+    expectedBookmarks = {((30, 30),), ((40, 40),)}
+    self.assertEqual(set(visibleBookmarks), expectedBookmarks)
+    self.assertEqual(len(visibleBookmarks), len(expectedBookmarks))
+
+    self.fakeHost.scrollRow = 21
+    self.lineNumbers.rows = 10
+    visibleBookmarks = self.lineNumbers.getVisibleBookmarks(
+        self.fakeHost.scrollRow, self.fakeHost.scrollRow + self.lineNumbers.rows)
+    expectedBookmarks = {((30, 30),)}
+    self.assertEqual(set(visibleBookmarks), expectedBookmarks)
+    self.assertEqual(len(visibleBookmarks), len(expectedBookmarks))
+
+    self.lineNumbers.rows = 9
+    visibleBookmarks = self.lineNumbers.getVisibleBookmarks(
+        self.fakeHost.scrollRow, self.fakeHost.scrollRow + self.lineNumbers.rows)
+    expectedBookmarks = {}
+    self.assertEqual(visibleBookmarks, [])
+
+    self.fakeHost.scrollRow = 10
+    self.textBuffer.bookmarks = [((0, 10),), ((11, 30),),
+                                 ((30, 45),), ((45, 49),)]
+    self.lineNumbers.rows = 15
+    import pdb; pdb.set_trace()
+    visibleBookmarks = self.lineNumbers.getVisibleBookmarks(
+        self.fakeHost.scrollRow, self.fakeHost.scrollRow + self.lineNumbers.rows)
+    expectedBookmarks = {((0, 10),), ((11, 30),)}
+    self.assertEqual(set(visibleBookmarks), expectedBookmarks)
+    self.assertEqual(len(visibleBookmarks), len(expectedBookmarks))
+
+
 
 
 
