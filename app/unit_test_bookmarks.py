097d5ebf4a69a405092608c01c00ed01ac170c2a
Aaron Xu <aaron_rg_xu@hotmail.com>
2018-01-16 11:38:30 -0800

    Refactored code to improve readability

--- app/unit_test_bookmarks.py
+++ app/unit_test_bookmarks.py
@@ -11,34 +11,24 @@ class BookmarkTestCases(unittest.TestCase):
     pass
 
   def test_get_next_bookmark_color(self):
-    # Test for 8-colored mode
-    patch.dict(app.prefs.startup, {'numColors': 8}, clear=True)
-    colors = set()
-    expectedNumberOfColors = 5
-    for _ in range(expectedNumberOfColors):
+    def test_with_an_x_colored_terminal(x):
+      patch.dict(app.prefs.startup, {'numColors': x}, clear=True)
+      colors = set()
+      expectedNumberOfColors = 5
+      for _ in range(expectedNumberOfColors):
+        color = self.textBuffer.getBookmarkColor()
+        # Make sure that a color index is returned.
+        self.assertEqual(type(color), int)
+        colors.add(color)
+      # Test that all colors were different.
+      self.assertEqual(len(colors), expectedNumberOfColors)
       color = self.textBuffer.getBookmarkColor()
-      # Make sure that a color index is returned.
-      self.assertEqual(type(color), int)
       colors.add(color)
-    # Test that all colors were different.
-    self.assertEqual(len(colors), expectedNumberOfColors)
-    color = self.textBuffer.getBookmarkColor()
-    colors.add(color)
-    # Test that the function rotates 5 colors.
-    self.assertEqual(len(colors), expectedNumberOfColors)
+      # Test that the function rotates 5 colors.
+      self.assertEqual(len(colors), expectedNumberOfColors)
 
-    # Test for 256-colored mode
-    patch.dict(app.prefs.startup, {'numColors': 256}, clear=True)
-    colors = set()
-    expectedNumberOfColors = 5
-    for _ in range(expectedNumberOfColors):
-      color = self.textBuffer.getBookmarkColor()
-      # Make sure that a color index is returned.
-      self.assertEqual(type(color), int)
-      colors.add(color)
-    # Test that all colors were different.
-    self.assertEqual(len(colors), expectedNumberOfColors)
-    color = self.textBuffer.getBookmarkColor()
-    colors.add(color)
-    # Test that the function rotates 5 colors.
-    self.assertEqual(len(colors), expectedNumberOfColors)
+      # Test for 8-colored mode
+      test_with_an_x_colored_terminal(8)
+
+      # Test for 256-colored mode
+      test_with_an_x_colored_terminal(256)
