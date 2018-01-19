0ea76b7ad35cc5fc16eb3baff9aa4fdc5cd5bf5a
Dave Schuyler <dschuyler@chromium.org>
2018-01-18 20:28:01 -0800

    open file window, highlight and filter

--- app/file_manager_controller.py
+++ app/file_manager_controller.py
@@ -16,6 +16,7 @@ from app.curses_util import *
 import app.buffer_manager
 import app.controller
 import os
+import re
 import time
 
 
@@ -24,6 +25,7 @@ class DirectoryListController(app.controller.Controller):
   """
   def __init__(self, host):
     app.controller.Controller.__init__(self, host, 'DirectoryListController')
+    self.filter = None
     self.shownDirectory = None
 
   def focus(self):
@@ -35,66 +37,59 @@ class DirectoryListController(app.controller.Controller):
 
   def onChange(self):
     input = self.host.getPath()
-    app.log.debug(input, "\n", self.shownDirectory)
     if self.shownDirectory == input:
       return
     self.shownDirectory = input
-    path = os.path.abspath(os.path.expanduser(os.path.expandvars(input)))
-    if os.path.isfile(path):
-      return
-      if not os.access(path, os.R_OK):
-        clip = [input + ":", 'Error opening file.']
-      else:
-        app.log.info('got a file', path)
-        textBuffer = app.buffer_manager.buffers.loadTextBuffer(path,
-            self.host.host.inputWindow)
-        self.host.host.inputWindow.setTextBuffer(textBuffer)
-        self.changeToInputWindow()
-        return
+    fullPath = os.path.abspath(os.path.expanduser(os.path.expandvars(input)))
+    dirPath = fullPath
+    fileName = ''
+    if len(input) > 0 and input[-1] != os.sep:
+      dirPath, fileName = os.path.split(fullPath)
+      self.host.textBuffer.findRe = re.compile('()^' + re.escape(fileName))
     else:
-      dirPath = path or '.'
-      fileName = ''
-      if len(input) > 0 and input[-1] != os.sep:
-        dirPath, fileName = os.path.split(path)
-      if os.path.isdir(dirPath):
-        lines = []
-        try:
-          fileLines = []
-          contents = os.listdir(dirPath)
-          for i in contents:
-            if not self.host.host.opt['dotFiles'] and i[0] == '.':
-              continue
-            fullPath = os.path.join(dirPath, i)
-            if os.path.isdir(fullPath):
-              i += os.path.sep
-            iSize = ''
-            iModified = ''
-            if self.host.host.opt['sizes'] and os.path.isfile(fullPath):
-              iSize = '%d bytes' % os.path.getsize(fullPath)
-            if self.host.host.opt['modified']:
-              iModified = os.path.getmtime(fullPath)
-            fileLines.append([i, iSize, iModified])
-          if self.host.opt['Size'] is not None:
-            # Sort by size.
-            fileLines.sort(reverse=not self.host.opt['Size'],
-                key=lambda x: x[1])
-          elif self.host.opt['Modified'] is not None:
-            # Sort by modification date.
-            fileLines.sort(reverse=not self.host.opt['Modified'],
-                key=lambda x: x[2])
-          else:
-            fileLines.sort(reverse=not self.host.opt['Name'],
-                key=lambda x: unicode.lower(x[0]))
-          lines = ['%-40s  %16s  %24s' % (
-              i[0], i[1], unicode(time.strftime('%c', time.localtime(i[2]))))
-              for i in fileLines]
-          self.host.contents = [i[0] for i in fileLines]
-        except OSError as e:
-          lines = ['Error opening directory.']
-          lines.append(unicode(e))
-        clip = ['./', '../'] + lines
-      else:
-        clip = [dirPath + ": not found"]
+      self.host.textBuffer.findRe = None
+    dirPath = dirPath or '.'
+    if os.path.isdir(dirPath):
+      lines = []
+      try:
+        fileLines = []
+        contents = os.listdir(dirPath)
+        for i in contents:
+          if not self.host.host.opt['dotFiles'] and i[0] == '.':
+            continue
+          if self.filter is not None and not i.startswith(self.filter):
+            continue
+          fullPath = os.path.join(dirPath, i)
+          if os.path.isdir(fullPath):
+            i += os.path.sep
+          iSize = ''
+          iModified = ''
+          if self.host.host.opt['sizes'] and os.path.isfile(fullPath):
+            iSize = '%d bytes' % os.path.getsize(fullPath)
+          if self.host.host.opt['modified']:
+            iModified = os.path.getmtime(fullPath)
+          fileLines.append([i, iSize, iModified])
+        if self.host.opt['Size'] is not None:
+          # Sort by size.
+          fileLines.sort(reverse=not self.host.opt['Size'],
+              key=lambda x: x[1])
+        elif self.host.opt['Modified'] is not None:
+          # Sort by modification date.
+          fileLines.sort(reverse=not self.host.opt['Modified'],
+              key=lambda x: x[2])
+        else:
+          fileLines.sort(reverse=not self.host.opt['Name'],
+              key=lambda x: unicode.lower(x[0]))
+        lines = ['%-40s  %16s  %24s' % (
+            i[0], i[1], unicode(time.strftime('%c', time.localtime(i[2]))))
+            for i in fileLines]
+        self.host.contents = [i[0] for i in fileLines]
+      except OSError as e:
+        lines = ['Error opening directory.']
+        lines.append(unicode(e))
+      clip = ['./', '../'] + lines
+    else:
+      clip = [dirPath + ": not found"]
     self.host.textBuffer.selectionAll()
     self.host.textBuffer.editPasteLines(tuple(clip))
     #self.host.textBuffer.findPlainText(fileName)
@@ -102,9 +97,14 @@ class DirectoryListController(app.controller.Controller):
     self.host.textBuffer.penCol = 0
     self.host.scrollRow = 0
     self.host.scrollCol = 0
+    self.filter = None
 
   def optionChanged(self, name, value):
     self.shownDirectory = None
+
+  def setFilter(self, filter):
+    self.filter = filter
+    self.shownDirectory = None  # Cause a refresh.
 
 
 class FileManagerController(app.controller.Controller):
@@ -158,8 +158,10 @@ class FileManagerController(app.controller.Controller):
 
   def tabCompleteExtend(self):
     """Extend the selection to match characters in common."""
-    dirPath, fileName = os.path.split(self.textBuffer.lines[0])
-    expandedDir = os.path.expandvars(os.path.expanduser(dirPath)) or '.'
+    expandedPath = os.path.expandvars(os.path.expanduser(
+        self.textBuffer.lines[0]))
+    dirPath, fileName = os.path.split(expandedPath)
+    expandedDir = dirPath or '.'
     matches = []
     if not os.path.isdir(expandedDir):
       return
@@ -190,4 +192,8 @@ class FileManagerController(app.controller.Controller):
       return prefixLen
     prefixLen = findCommonPrefixLength(len(fileName))
     self.textBuffer.insert(matches[0][len(fileName):prefixLen])
+    if expandedPath == os.path.expandvars(os.path.expanduser(
+        self.textBuffer.lines[0])):
+      # No further expansion found.
+      self.host.directoryList.controller.setFilter(fileName)
     self.onChange()
