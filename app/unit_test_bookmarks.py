9e9954b2e45df9244edb20e03a5f20d84415a05d
Aaron Xu <aaron_rg_xu@hotmail.com>
2018-03-05 09:16:57 -0800

    Rebased to master

--- app/unit_test_bookmarks.py
+++ app/unit_test_bookmarks.py
@@ -12,6 +12,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+import unittest
+
 from app.bookmark import Bookmark
 import app.prefs
 import app.text_buffer
