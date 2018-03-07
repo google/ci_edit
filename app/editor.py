2948a7b2d1f510b9e625cebd4cfb3316faf3449c
Dave Schuyler <dschuyler@chromium.org>
2018-03-06 20:13:38 -0800

    unicode in goto line

--- app/editor.py
+++ app/editor.py
@@ -246,8 +246,8 @@ class InteractiveGoto(app.controller.Controller):
 
   def onChange(self):
     app.log.info()
-    line = ''
+    line = U''
     try: line = self.textBuffer.lines[0]
     except: pass
-    gotoLine, gotoCol = (line.split(',') + ['0', '0'])[:2]
+    gotoLine, gotoCol = (line.split(U',') + [U'0', U'0'])[:2]
     self.cursorMoveTo(parseInt(gotoLine)-1, parseInt(gotoCol))
