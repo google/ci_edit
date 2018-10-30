4a5e59072eca60cf735e6f7c31c44ef81e739aa4
Dave Schuyler <dschuyler@chromium.org>
2018-10-30 11:00:32 -0700

    Revert "removing dirs"
    This reverts commit df680c8b3b36ba7e5c7e0e1379108fb357223b68.

---
+++ test_fake/curses/constants.py
@@ -0,0 +1,121 @@
+# Copyright 2017 Google Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+
+# These constants have specific values.
+COLORS = 256
+ERR = 0  # ERR is normally -1, but bg signal doesn't work in unit test.
+
+
+
+# These constants have arbitrary values.
+KEY_ALT_A = 0  # todo
+KEY_ALT_B = 1  # todo
+KEY_ALT_C = 2  # todo
+KEY_ALT_LEFT = (91, 49, 59, 57, 68)
+KEY_ALT_RIGHT = (91, 49, 59, 57, 67)
+KEY_ALT_S = 5  # todo
+KEY_ALT_SHIFT_LEFT = 6  # todo
+KEY_ALT_SHIFT_RIGHT = 7  # todo
+KEY_BACKSPACE = 8  # todo
+KEY_BACKSPACE1 = 9  # todo
+KEY_BACKSPACE2 = 10  # todo
+KEY_BACKSPACE3 = 11  # todo
+KEY_BTAB = 12  # todo
+KEY_CTRL_DOWN = 13  # todo
+KEY_CTRL_LEFT = 14  # todo
+KEY_CTRL_RIGHT = 15  # todo
+KEY_CTRL_SHIFT_DOWN = 16  # todo
+KEY_CTRL_SHIFT_LEFT = 17  # todo
+KEY_CTRL_SHIFT_RIGHT = 18  # todo
+KEY_CTRL_SHIFT_UP = 19  # todo
+KEY_CTRL_UP = 20  # todo
+KEY_DC = 21  # todo
+KEY_DELETE = 22  # todo
+KEY_DOWN = 23  # todo
+KEY_END = 24  # todo
+KEY_ESCAPE = 27
+KEY_F1 = 265
+KEY_F10 = 274
+KEY_F13 = 28  # todo
+KEY_F14 = 278
+KEY_F15 = 30  # todo
+KEY_F16 = 31  # todo
+KEY_F17 = 32  # todo
+KEY_F18 = 33  # todo
+KEY_F19 = 34  # todo
+KEY_F2 = 266
+KEY_F20 = 36  # todo
+KEY_F21 = 37  # todo
+KEY_F22 = 38  # todo
+KEY_F3 = 267
+KEY_F4 = 268
+KEY_F5 = 41  # todo
+KEY_F6 = 42  # todo
+KEY_F7 = 43  # todo
+KEY_F8 = 44  # todo
+KEY_F9 = 45  # todo
+KEY_HOME = 46  # todo
+KEY_LEFT = 260
+KEY_MOUSE = 409
+KEY_NPAGE = 49  # todo
+KEY_PAGE_DOWN = 50  # todo
+KEY_PAGE_UP = 51  # todo
+KEY_PPAGE = 52  # todo
+KEY_RESIZE = 53  # todo
+KEY_RESIZE = 54  # todo
+KEY_RIGHT = 261  # todo
+KEY_SF = 56  # todo
+KEY_SHIFT_DOWN = 57  # todo
+KEY_SHIFT_F2 = 58  # todo
+KEY_SHIFT_F3 = 59  # todo
+KEY_SHIFT_LEFT = 60  # todo
+KEY_SHIFT_RIGHT = 61  # todo
+KEY_SHIFT_UP = 62  # todo
+KEY_SLEFT = 63  # todo
+KEY_SNEXT = 64  # todo
+KEY_SPREVIOUS = 65  # todo
+KEY_SR = 66  # todo
+KEY_SRIGHT = 67  # todo
+KEY_UP = 68  # todo
+KEY_SEND = 69  # todo
+KEY_SHOME = 70  # todo
+
+
+A_BOLD = 1000
+A_REVERSE = 1001
+BUTTON1_CLICKED = 1
+BUTTON1_DOUBLE_CLICKED = 2
+BUTTON1_PRESSED = 3
+BUTTON1_RELEASED = 4
+BUTTON2_CLICKED = 5
+BUTTON2_DOUBLE_CLICKED = 6
+BUTTON2_PRESSED = 7
+BUTTON2_RELEASED = 8
+BUTTON3_CLICKED = 9
+BUTTON3_DOUBLE_CLICKED = 10
+BUTTON3_PRESSED = 11
+BUTTON3_RELEASED = 12
+BUTTON4_CLICKED = 13
+BUTTON4_DOUBLE_CLICKED = 14
+BUTTON4_PRESSED = 15
+BUTTON4_RELEASED = 16
+BUTTON_ALT = 17
+BUTTON_CTRL = 18
+BUTTON_SHIFT = 19
+REPORT_MOUSE_POSITION = 100
