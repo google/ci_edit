5af80d643e7b16e5e3270771bdbc6b322255d460
Dave Schuyler <dschuyler@chromium.org>
2018-11-28 10:55:10 -0800

    [rust] reporting mouse events

--- rust/src/ci_program.rs
+++ rust/src/ci_program.rs
@@ -18,6 +18,30 @@ extern crate ncurses;
 
 const KEY_CTRL_Q: i32 = 17;
 
+struct CiProgram {
+    debug_mouse_event: ncurses::ll::MEVENT,
+    exiting: bool,
+    curses_screen: ncurses::WINDOW,
+    ch: i32,
+}
+
+impl CiProgram {
+    pub fn new() -> CiProgram {
+        CiProgram {
+            debug_mouse_event: ncurses::ll::MEVENT {
+                id: 0, x: 0, y: 0, z: 0, bstate: 0,
+            },
+            exiting: false,
+            curses_screen: ncurses::initscr(),
+            ch: 0,
+        }
+    }
+
+    pub fn quit_now(&mut self) {
+        self.exiting = true;
+    }
+}
+
 pub fn enable_bracketed_paste() -> std::io::Result<()> {
     // Enable Bracketed Paste Mode.
     let stdout = std::io::stdout();
@@ -33,24 +57,39 @@ pub fn run() {
     // Maybe set to "en_US.UTF-8"?
     ncurses::setlocale(ncurses::LcCategory::all, "");
 
-    ncurses::initscr();
+    let mut ci_program = CiProgram::new();
     ncurses::raw();
     ncurses::mousemask(ncurses::ALL_MOUSE_EVENTS as ncurses::mmask_t, None);
     ncurses::keypad(ncurses::stdscr(), true);
     ncurses::meta(ncurses::stdscr(), true);
     ncurses::noecho();
     enable_bracketed_paste().expect("enable_bracketed_paste failed");
+    ncurses::start_color();
+    ncurses::timeout(10);
+    let curses_window = ci_program.curses_screen;
+    ncurses::leaveok(curses_window, true);  // Don't update cursor position.
+    ncurses::scrollok(curses_window, true);
+    ncurses::keypad(curses_window, true);
+    //app.window.mainCursesWindow = curses_window
 
     ncurses::clear();
     ncurses::mv(0, 0);
     ncurses::printw("This is a work in progress\n");
     ncurses::printw("Press ctrl+q to exit.\n");
-    loop {
+    while ci_program.exiting == false {
         let c = ncurses::getch();
+        ci_program.ch = c;
         if c == KEY_CTRL_Q {
-            break;
+            ci_program.quit_now();
         }
-        ncurses::printw(&format!("pressed {}\n", c));
+        if c == 409 {
+            let _err = ncurses::getmouse(&mut ci_program.debug_mouse_event);
+            ncurses::printw(&format!("mouse {:?}\n",
+                ci_program.debug_mouse_event));
+        }
+        if c >= 0 {
+            ncurses::printw(&format!("pressed {}\n", ci_program.ch));
+        }
     }
     ncurses::endwin();
 }
