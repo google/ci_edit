e36132fe19064b7617a26f896f69277b458f3bb1
Dave Schuyler <dschuyler@chromium.org>
2018-11-28 20:31:26 -0800

    [rust] wip on ci_program

--- rust/src/ci_program.rs
+++ rust/src/ci_program.rs
@@ -41,12 +41,94 @@ impl CiProgram {
         }
     }
 
+    pub fn init(&mut self) {
+        // Maybe set to "en_US.UTF-8"?
+        ncurses::setlocale(ncurses::LcCategory::all, "");
+
+        //let mut ci_program = CiProgram::new();
+        ncurses::raw();
+        ncurses::mousemask(ncurses::ALL_MOUSE_EVENTS as ncurses::mmask_t, None);
+        ncurses::keypad(ncurses::stdscr(), true);
+        ncurses::meta(ncurses::stdscr(), true);
+        ncurses::noecho();
+        enable_bracketed_paste().expect("enable_bracketed_paste failed");
+        ncurses::start_color();
+        ncurses::timeout(10);
+        let curses_window = self.curses_screen;
+        ncurses::leaveok(curses_window, true); // Don't update cursor position.
+        ncurses::scrollok(curses_window, true);
+        ncurses::keypad(curses_window, true);
+        //app.window.mainCursesWindow = curses_window
+    }
+
+    pub fn command_loop(&mut self) {
+        ncurses::clear();
+        ncurses::mv(0, 0);
+        ncurses::printw("This is a work in progress\n");
+        ncurses::printw("Press ctrl+q to exit.\n");
+        while self.exiting == false {
+            let c = ncurses::getch();
+            self.ch = c;
+            if c == KEY_CTRL_Q {
+                self.quit_now();
+            }
+            if c == 409 {
+                let _err = ncurses::getmouse(&mut self.debug_mouse_event);
+                ncurses::printw(&format!(
+                    "mouse {:?}\n",
+                    self.debug_mouse_event
+                ));
+            }
+            if c >= 0 {
+                ncurses::printw(&format!("pressed {}\n", self.ch));
+            }
+        }
+        ncurses::endwin();
+    }
+
+    pub fn parse_args(&mut self) {}
+
     pub fn quit_now(&mut self) {
         self.exiting = true;
     }
+
+    pub fn run(&mut self) {
+        self.init();
+
+        self.parse_args();
+        self.set_up_palette();
+        /*
+        homePath = app.prefs.prefs['userData'].get('homePath')
+        self.makeHomeDirs(homePath)
+        app.history.loadUserHistory()
+        app.curses_util.hackCursesFixes()
+        if app.prefs.editor['useBgThread']:
+          self.bg = app.background.startupBackground()
+        self.startup()
+        if app.prefs.startup.get('profile'):
+          profile = cProfile.Profile()
+          profile.enable()
+          self.commandLoop();
+          profile.disable()
+          output = io.StringIO.StringIO()
+          stats = pstats.Stats(profile, stream=output).sort_stats('cumulative')
+          stats.print_stats()
+          app.log.info(output.getvalue())
+        else:
+        */
+        self.command_loop();
+        /*
+        if app.prefs.editor['useBgThread']:
+          self.bg.put((self.programWindow, 'quit'))
+          self.bg.join()
+        */
+        ncurses::endwin();
+    }
+
+    pub fn set_up_palette(&mut self) {}
 }
 
-pub fn enable_bracketed_paste() -> std::io::Result<()> {
+fn enable_bracketed_paste() -> std::io::Result<()> {
     // Enable Bracketed Paste Mode.
     let stdout = std::io::stdout();
     let mut stdout_handle = stdout.lock();
@@ -57,45 +139,9 @@ pub fn enable_bracketed_paste() -> std::io::Result<()> {
     return Ok(());
 }
 
-pub fn run() {
-    // Maybe set to "en_US.UTF-8"?
-    ncurses::setlocale(ncurses::LcCategory::all, "");
-
+pub fn run_ci() {
+    // Reduce the delay waiting for escape sequences.
+    std::env::set_var("ESCDELAY", "1");
     let mut ci_program = CiProgram::new();
-    ncurses::raw();
-    ncurses::mousemask(ncurses::ALL_MOUSE_EVENTS as ncurses::mmask_t, None);
-    ncurses::keypad(ncurses::stdscr(), true);
-    ncurses::meta(ncurses::stdscr(), true);
-    ncurses::noecho();
-    enable_bracketed_paste().expect("enable_bracketed_paste failed");
-    ncurses::start_color();
-    ncurses::timeout(10);
-    let curses_window = ci_program.curses_screen;
-    ncurses::leaveok(curses_window, true); // Don't update cursor position.
-    ncurses::scrollok(curses_window, true);
-    ncurses::keypad(curses_window, true);
-    //app.window.mainCursesWindow = curses_window
-
-    ncurses::clear();
-    ncurses::mv(0, 0);
-    ncurses::printw("This is a work in progress\n");
-    ncurses::printw("Press ctrl+q to exit.\n");
-    while ci_program.exiting == false {
-        let c = ncurses::getch();
-        ci_program.ch = c;
-        if c == KEY_CTRL_Q {
-            ci_program.quit_now();
-        }
-        if c == 409 {
-            let _err = ncurses::getmouse(&mut ci_program.debug_mouse_event);
-            ncurses::printw(&format!(
-                "mouse {:?}\n",
-                ci_program.debug_mouse_event
-            ));
-        }
-        if c >= 0 {
-            ncurses::printw(&format!("pressed {}\n", ci_program.ch));
-        }
-    }
-    ncurses::endwin();
+    ci_program.run();
 }
