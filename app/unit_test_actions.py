7d5c77173841141e14ea0f013645fbfe7ed41852
Saurav Chirania <saurav.chir@gmail.com>
2019-01-21 17:05:10 +0530

    Add test cases for status line updation on changing selection mode

--- app/unit_test_actions.py
+++ app/unit_test_actions.py
@@ -294,3 +294,12 @@ class TextDeleteTestCases(unittest.TestCase):
         self.assertEqual(tb.message[0], u"1 characters (1 lines) selected")
         tb.carriageReturn()
         self.assertEqual(tb.message, None)
+
+    def test_message_after_text_deletion(self):
+        tb = self.textBuffer
+        self.assertEqual(tb.message[0], u"New buffer")
+        tb.insert(u"p")
+        tb.selectionAll()
+        self.assertEqual(tb.message[0], u"1 characters (1 lines) selected")
+        tb.insert("q")
+        self.assertEqual(tb.message, None)
